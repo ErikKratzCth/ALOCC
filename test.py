f045e43f7cd754c8ada4ebf292f5ef895f1b1676
ErikKratzCth <erik.kj.kratz@gmail.com>
2018-10-15 16:54:00 +0200

    added nstride flag

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ import time
 import os
 
 flags = tf.app.flags
+flags.DEFINE_integer("nStride",1,"nStride ?[1]")
 flags.DEFINE_integer("epoch", 1, "Epoch to train [25]")
 flags.DEFINE_float("learning_rate", 0, "Learning rate of for adam [0.0002]")
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
